fe11d8572f4b793cf609b34be69faa1f5cc48d49
Elezetabe <laurita_9505@hotmail.com>
2025-06-16 12:43:10 -0500

    Corrección de errores de los botones Agregar Detalle y Limpiar

--- src/components/FacturaCompraForm.vue
+++ src/components/FacturaCompraForm.vue
@@ -10,29 +10,38 @@
     <v-btn color="secondary" class="ml-4" :disabled="!localForm.id" @click="$emit('open-detalle')">
       Agregar Detalle
     </v-btn>
-    <v-btn variant="text" @click="$emit('reset')">Limpiar</v-btn>
+    <v-btn variant="text" @click="emitReset">Limpiar</v-btn>
   </v-form>
 </template>
 
 <script setup lang="ts">
 import { ref, watch } from "vue";
 const props = defineProps({
-  form: Object,
+  form: { type: Object, default: () => ({}) },
   isEditing: Boolean,
 });
 const emit = defineEmits(["submit", "reset", "open-detalle"]);
 
 const localForm = ref({ ...props.form });
 
+// Sincroniza localForm con form del padre
 watch(
   () => props.form,
   (val) => {
-    Object.assign(localForm.value, val);
+    Object.assign(localForm.value, val || {});
+    // Si quieres limpiar completamente los campos, puedes hacer:
+    // localForm.value = { ...val };
   },
   { deep: true }
 );
 
 function emitSubmit() {
   emit("submit", { ...localForm.value });
+}
+
+function emitReset() {
+  // Limpia el formulario local y notifica al padre
+  localForm.value = {};
+  emit("reset");
 }
 </script>
